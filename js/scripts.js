dcf7270820f57f695c2df0700009c518e10dee8e
Spencer Moody <spencer.moody@outlook.com>
2020-06-02 11:01:38 -0700

    click and addClass method and styles to background colors
    Co-authored-by: Taylor Phillips <taylorphillips133@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("button#green").click(function(){
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(functions(){
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(functions(){
+    $("body").addClass("red-background");
+  });
+});
