e8ab982182a2ed1fc79e147ca6925312c7f608cb
Spencer Moody <spencer.moody@outlook.com>
2020-06-02 11:20:11 -0700

    Add removeClass method to body
    Co-authored-by: Taylor Phillips <taylorphillips133@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-  $("button#green").click(function(){
+  $("button#green").click(function() {
+    $("body").removeClass();
     $("body").addClass("green-background");
   });
 
-  $("button#yellow").click(functions(){
+  $("button#yellow").click(function() {
+    $("body").removeClass();
     $("body").addClass("yellow-background");
   });
 
-  $("button#red").click(functions(){
+  $("button#red").click(function() {
+    $("body").removeClass();
     $("body").addClass("red-background");
   });
 });
